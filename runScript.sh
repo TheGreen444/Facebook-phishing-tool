c4e22182c0762b58f1c16544952262a8c7e34398
~ TheGreen H4ck3r ~ <146537118+TheGreen444@users.noreply.github.com>
2024-03-29 00:29:28 -0700

    runScript.sh

--- runScript.sh
+++ runScript.sh
@@ -32,7 +32,7 @@ display_menu() {
 
 # Function to handle user input
 handle_input() {
-   read -p $'\e[38;2;0;255;0mTheGreen☠️ H4ck3r ~# \e[0m' choice
+   read -p $'\e[38;2;0;255;0mTheGreen☠️H4ck3r ~# \e[0m' choice
     case $choice in
         1)
             python3 Tool/runScript.py
